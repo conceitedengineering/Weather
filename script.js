c700cbe4707ef978c47ab7a644218f3712c2d5e7
Negash <negash@cca.edu>
2023-12-12 13:05:21 -0800

    ONG

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-document.addEventListener('DOMContentLoaded', fetchWeather);
+// Replace 'YOUR_API_KEY' with your actual OpenWeather API key
+const apiKey = process.env.WEATHER_API_KEY;
+const city = 'San Francisco'; // Replace with the city you want the weather for
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-function fetchWeather() {
-  // Use a relative path if your frontend is hosted on the same domain as your serverless function
-  fetch('https://weather-seven-steel.vercel.app/')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok: ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      displayWeather(data);
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation: ', error);
-      document.getElementById('weather').innerText = 'Weather data unavailable';
-    });
-}
 
-function displayWeather(data) {
-  // Assuming your serverless function returns the weather data in the desired format
-  const mainWeather = data.mainWeather;
-  const description = data.description;
-  const temp = Math.round(data.temp);
-
-  const weatherElement = document.getElementById('weather');
-  const weatherDescriptionElement = document.getElementById('weather-description');
-
-  weatherElement.innerText = `${mainWeather} ${temp}°C`;
-
-  // Display the description if it does not directly match the 'main' weather
-  if (description.toLowerCase() !== mainWeather.toLowerCase()) {
-    weatherDescriptionElement.innerText = description;
-  }
-}
+const weatherElement = document.getElementById('weather');
+const weatherDescriptionElement = document.getElementById('weather-description');
 
 function capitalize(str) {
   return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
+
+function fetchWeather() {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const mainWeather = data.weather[0].main;
+      const description = data.weather[0].description;
+      const temp = Math.round(data.main.temp);
+
+      weatherElement.innerText = `${mainWeather} ${temp}°C`;
+
+      // Display the description if it does not directly match the 'main' weather
+      if (description.toLowerCase() !== mainWeather.toLowerCase()) {
+        weatherDescriptionElement.innerText = capitalize(description);
+      }
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+      weatherElement.innerText = 'Weather data unavailable';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', fetchWeather);
