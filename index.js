e3a8b4f722e865720641ad89a69a80d38a9eea8e
Negash <negash@cca.edu>
2023-12-20 09:24:14 -0800

    FFUCK

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Required modules
 const express = require('express');
 const fetch = require('node-fetch');
 require('dotenv').config();
@@ -10,15 +9,27 @@ const port = process.env.PORT || 3000;
 app.get('/weather', async (req, res) => {
   const city = req.query.city || 'San Francisco';
   const apiKey = process.env.API_KEY;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  // Validate API Key
+  if (!apiKey) {
+    return res.status(500).send('API key not configured');
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
 
   try {
     const response = await fetch(url);
+
+    // Check if the external API call was successful
+    if (!response.ok) {
+      throw new Error(`Error from OpenWeather API: ${response.status} ${response.statusText}`);
+    }
+
     const data = await response.json();
-    res.send(data);
+    res.json(data); // Use json() for consistency in response format
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching weather data');
+    console.error('Fetch error:', error.message);
+    res.status(500).send('Error fetching weather data: ' + error.message);
   }
 });
 
